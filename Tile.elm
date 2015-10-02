4073da5a8e078ace5dc19911fba6879aef0c61d6
Sergey Bolshchikov <sergey@bolshchikov.net>
2015-10-02 19:30:30 +0500

    chore(*): enable mark action

--- Tile.elm
+++ Tile.elm
@@ -1,6 +1,7 @@
 module Tile (Model, init, makeMine, Action, update, view) where
 
 import Html exposing (..)
+import Utils exposing (onRightClick)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
@@ -8,14 +9,16 @@ import Html.Events exposing (..)
 type alias Model =
   { isMine : Bool
   , isOpened : Bool
+  , isMarked : Bool
   }
 
-type Action = Reveal
+type Action = Reveal | Mark
 
 init : Bool -> Model
 init isMine =
   { isMine = isMine
   , isOpened = False
+  , isMarked = False
   }
 
 
@@ -25,22 +28,40 @@ makeMine model =
     isMine <- True
   }
 
+reveal : Model -> Model
+reveal model =
+  { model |
+    isOpened <- True
+  }
+
+mark : Model -> Model
+mark model =
+  if model.isMarked
+    then { model | isMarked <- False }
+    else { model | isMarked <- True }
+
+
 update : Action -> Model -> Model
 update action model =
   case action of
     Reveal ->
-      { model |
-        isOpened <- True
-      }
+      reveal model
+    Mark ->
+      mark model
+
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  let textValue
-    = toString model.isMine
-  in
-    if model.isOpened
-      then div [ class "tile" ] [ text textValue ]
-      else div [ class "tile"
-               , onClick address Reveal
-               ]
-               [ text "Click Me!" ]
+  if | model.isOpened ->
+        div [ class "tile" ] [ text << toString <| model.isMine ]
+     | model.isMarked ->
+        div [ class "tile marked"
+            , onClick address Reveal
+            , onRightClick address Mark
+            ] [ text "Marked" ]
+     | otherwise  ->
+        div [ class "tile"
+            , onClick address Reveal
+            , onRightClick address Mark
+            ]
+            [ text "Click Me!" ]
